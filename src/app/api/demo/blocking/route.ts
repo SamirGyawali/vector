927f1fb00df37b59c435d79e571b8638439005be
Samir Ubuntu <gyawali.samir000@gmail.com>
2026-03-07 17:03:53 +0545

    chore: setup inngest client & demo functions

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,14 @@
+// POST = in function defination
+// localhost:3000/api/demo/blocking
+
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+export async function POST() {
+  const response = await generateText({
+    model: google("gemini-2.5-flash"),
+    prompt: "generate something to read.",
+  });
+
+  return Response.json({ response });
+}
