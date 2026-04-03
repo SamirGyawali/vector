547f9fd10246be93a27b727a6b360efd77c407c8
Samir Ubuntu <gyawali.samir000@gmail.com>
2026-04-03 15:31:54 +0545

    07: Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,15 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { useAuth } from "@clerk/nextjs";
-import * as Sentry from "@sentry/nextjs";
+import { ProjectsView } from "@/features/projects/components/project-view";
 
-const DemoPage = () => {
-  const { userId } = useAuth();
 
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-
-  const [loading, setLoading] = useState(false);
-  const [loading2, setLoading2] = useState(false);
-  const handleBlocking = async () => {
-    setLoading(true);
-    await fetch("/api/demo/blocking", { method: "POST" });
-    setLoading(false);
-  };
-
-  const handleBackground = async () => {
-    setLoading2(true);
-    await fetch("/api/demo/background", { method: "POST" });
-    setLoading2(false);
-  };
-
-  const handleClientError = async () => {
-    // this is inside the logs in sentry
-    Sentry.logger.info("User attempting to click on client function", {
-      userId,
-    });
-    throw new Error("Client error: Something went wrong in browser!");
-  };
-
-  const handleAPIError = async () => {
-    await fetch("/api/demo/error", { method: "POST" });
-  };
-
-  const handleInngestError = async () => {
-    await fetch("/api/demo/inngest-error", { method: "POST" });
-  };
+const Home = () => {
 
   return (
     <div className="flex flex-col gap-3">
-      <div className="flex gap-3">
-        <Button
-          onClick={() =>
-            createProject({
-              name: "New project123",
-            })
-          }
-        >
-          Add new
-        </Button>
-
-        <Button disabled={loading} onClick={handleBlocking}>
-          {loading ? "Loading..." : "Blocking Job"}
-        </Button>
-        <Button disabled={loading2} onClick={handleBackground}>
-          {loading2 ? "Loading..." : "Background Job"}
-        </Button>
-        <Button variant="destructive" onClick={handleClientError}>
-          Client Error
-        </Button>
-        <Button variant="destructive" onClick={handleAPIError}>
-          Server Error
-        </Button>
-        <Button variant="destructive" onClick={handleInngestError}>
-          Inngest Error
-        </Button>
-      </div>
-      {projects?.map((project) => (
-        <div key={project._id}>
-          <p>{project.name}</p>
-          <p>{project.ownerId}</p>
-        </div>
-      ))}
+      <ProjectsView />
     </div>
   );
 };
 
-export default DemoPage;
+export default Home;
---
+++ src/features/projects/components/project-view.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { SparkleIcon } from "lucide-react";
+import { Kbd } from "@/components/ui/kbd";
+import { FaGithub } from "react-icons/fa";
+import { ProjectsList } from "@/features/projects/components/projects-list";
+import { useCreateProject } from "../hooks/use-projects";
+import {
+  uniqueNamesGenerator,
+  Config,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+import { useEffect, useState } from "react";
+import { ProjectsCommandDialog } from "./projects-command-dialog";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+export const ProjectsView = () => {
+  const createProject = useCreateProject();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.metaKey || e.ctrlKey) {
+        if (e.key === "k") {
+          e.preventDefault();
+          setCommandDialogOpen(true);
+        }
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const [commandDialogOpen, setCommandDialogOpen] = useState(false);
+
+  return (
+    <>
+      <ProjectsCommandDialog
+        open={commandDialogOpen}
+        onOpenChange={setCommandDialogOpen}
+      />
+      <div className="min-h-screen bg-slidebar flex flex-col items-center justify-center p-6 md:p-16">
+        <div className="w-full max-w-sm mx-auto flex flex-col gap-5 items-center">
+          <div className="flex justify-between gap-4 w-full items-center">
+            <div className="flex items-center gap-2 w-full group/logo">
+              <img
+                src="/vercel.svg"
+                alt="vector"
+                className="size-[12px] md:size-[46px]"
+              />
+              <h1
+                className={cn(
+                  "text-4xl md:text-5xl font-semibold",
+                  font.className,
+                )}
+              >
+                Vector
+              </h1>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 w-full">
+            <div className="grid grid-cols-2 gap-2">
+              <Button
+                variant="outline"
+                onClick={() => {
+                  const projectName = uniqueNamesGenerator({
+                    dictionaries: [adjectives, animals, colors],
+                    separator: "-",
+                    length: 3,
+                  });
+                  createProject({
+                    name: projectName,
+                  });
+                }}
+                className="h-full items-start justify-start p-4 bg-background border flex flex-col gap-6 rounded-none"
+              >
+                <div className="flex items-center justify-between w-full">
+                  <SparkleIcon className="size-4" />
+                  <Kbd className="bg-accent border">Ctrl+J</Kbd>
+                </div>
+                <div>
+                  <span>New</span>
+                </div>
+              </Button>
+              <Button
+                variant="outline"
+                onClick={() => {}}
+                className="h-full items-start justify-start p-4 bg-background border flex flex-col gap-6 rounded-none"
+              >
+                <div className="flex items-center justify-between w-full">
+                  <FaGithub className="size-4" />
+                  <Kbd className="bg-accent border">Ctrl+I</Kbd>
+                </div>
+                <div>
+                  <span>Import</span>
+                </div>
+              </Button>
+            </div>
+          </div>
+          <div className="w-full">
+            <ProjectsList onViewAll={() => setCommandDialogOpen(true)} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
