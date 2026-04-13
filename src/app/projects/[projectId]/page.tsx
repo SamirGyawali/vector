cc8270bfe47ebc844369de7a4aa4b5f9c09abfb2
Samir Ubuntu <gyawali.samir000@gmail.com>
2026-04-13 16:00:03 +0545

    feat: added allotments, project layout

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,12 +1,14 @@
+import { ProjectIdView } from "@/features/projects/components/project-id-view";
 import React from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 const ProjectIdPage = async ({
   params,
 }: {
-  params: Promise<{ projectId: string }>;
+  params: Promise<{ projectId: Id<"projects"> }>;
 }) => {
   const { projectId } = await params;
-  return <div>{projectId}</div>;
+  return <ProjectIdView projectId={projectId} />;
 };
 
 export default ProjectIdPage;
